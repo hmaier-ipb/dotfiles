d6da6038b2732b4fb341332b062dcd13cbf1bb07
Hendrik Maier <hmaier@ipb-halle.de>
2022-02-15 09:56:58 +0100

    adding some script

---
+++ workspace/shell/edit-config-files.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+declare -a config_files
+
+config_files=(
+	"shell scripts"
+	"$HOME/.config/bspwm/bspwmrc" 
+	"$HOME/.config/sxhkd/sxhkdrc"
+	"$HOME/.bash_aliases"
+	"$HOME/.bashrc"
+	"$HOME/.vimrc"
+	"$HOME/.config/polybar/config"
+	"$HOME/.config/nvim/init.vim"
+	) 
+
+# single and double quotes are different in bash
+# the @ takes every array items as an single argument
+# the * would match them all together
+choice=$(printf '%s\n' "${config_files[@]}" | dmenu -i -l 10 -p "Which config file to edit?")
+
+[[ "$choice" == "" ]] && exit 0
+[[ "$choice" == "shell scripts" ]] && xfce4-terminal --working-directory=/home/$USER/workspace/shell && exit 0
+
+xfce4-terminal --maximize --command="nvim $choice"
