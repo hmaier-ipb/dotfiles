f7f96da3d1dc9b125a99925b552bacc0ea3e83f9
Hendrik Maier <hmaier@ipb-halle.de>
2022-04-27 07:15:54 +0200

    geändert:       ../../.config/bspwm/bspwmrc 	gelöscht:       change-background.sh 	gelöscht:       edit-config-files.sh 	gelöscht:       polybar-pacman-updates.sh

--- workspace/shell/change-background.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-path="$HOME/Bilder/backgrounds"
-
-choice=$(ls -1 $path | dmenu -l 10 -p "Choose a new background: ")
-
-feh --no-fehbg --bg-scale "$path/$choice" 
--- workspace/shell/polybar-pacman-updates.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if ! updates=$(checkupdates 2> /dev/null | wc -l ); then
-    updates=0
-else
-	updates=$(checkupdates | wc -l )
-fi
-echo "$updates updates"
-
