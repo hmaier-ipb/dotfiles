42438f4d1a1d7db24a1d19709d9345666fe915d1
Hendrik Maier <hmaier@ipb-halle.de>
2022-04-27 07:29:43 +0200

    geändert:       .config/bspwm/bspwmrc 	geändert:       .config/polybar/config 	geändert:       .config/sxhkd/sxhkdrc 	geändert:       workspace/shell/production/change-background.sh

--- workspace/shell/production/change-background.sh
+++ workspace/shell/production/change-background.sh
@@ -4,4 +4,6 @@ path="$HOME/Bilder/backgrounds"
 
 choice=$(ls -1 $path | dmenu -l 10 -p "Choose a new background: ")
 
+echo "$path/$choice" > "$path/.active-background"
+
 feh --no-fehbg --bg-scale "$path/$choice" 
