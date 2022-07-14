9272d81a52ddf2374935d0026f4bead2d0d248f7
Hendrik Maier <hmaier@ipb-halle.de>
2022-07-14 07:24:50 +0200

    checking if  is empty to prevent errors

--- .scripts/change-background.sh
+++ .scripts/change-background.sh
@@ -4,6 +4,8 @@ path="$HOME/Bilder/backgrounds"
 
 choice=$(ls -1 $path | dmenu -l 10 -p "Choose a new background: ")
 
-echo "$path/$choice" > "$path/.active-background.txt"
+if [[ -n "$choice" ]]; then # choice is not empty
+	echo "$path/$choice" > "$path/.active-background.txt"
+	feh --no-fehbg --bg-scale "$path/$choice" 
+fi
 
-feh --no-fehbg --bg-scale "$path/$choice" 
