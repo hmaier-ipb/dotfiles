8ce3022eb7d238439f17b7e635e5fb2413d379b1
Hendrik Maier <hmaier@ipb-halle.de>
2022-07-15 12:56:11 +0200

    changed bspwmrc and added uptime-log.sh

---
+++ .scripts/uptime-log.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Make this as an alias for reboot & poweroff,
+# so the complete uptime per day is logged
+log="$HOME/.local/logs/worktime.log"
+time=$(uptime -p)
+date=$(date)
+# TODO: improve formatting with sed
+echo "$date $time minutes" >> $log
+
+cp $log $HOME/networkshare/hmaier/logs/
+
+
